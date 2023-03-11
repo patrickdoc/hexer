de3cf6ea52226a4b83590bc63a2c51d7dd64afa8
Patrick Dougherty <patrick.doc@ameritech.net>
2023-03-11 13:26:09 -0600

    working

---
+++ app/Lexer.hs
@@ -0,0 +1,138 @@
+module Lexer where
+
+import Language.Haskell.HLint
+import GHC.Hs
+import GHC.Types.SrcLoc
+import GHC.Utils.Outputable (defaultSDocContext, printSDocLn, sdocPprDebug)
+import GHC.Utils.Ppr
+import System.Environment
+import System.IO
+
+-- Flatparse
+import FlatParse.Basic hiding (Parser, runParser, string, char, cut)
+import qualified FlatParse.Basic as FP
+import qualified Data.ByteString as B
+import Data.String
+import "template-haskell" Language.Haskell.TH
+
+type Parser = FP.Parser Error
+
+-- | An expected item which is displayed in error messages.
+data Expected
+  = Msg String  -- ^ An error message.
+  | Lit String  -- ^ A literal expected thing.
+  deriving (Eq, Show, Ord)
+
+instance IsString Expected where fromString = Lit
+
+-- | A parsing error.
+data Error
+  = Precise Pos Expected     -- ^ A precisely known error, like leaving out "in" from "let".
+  | Imprecise Pos [Expected] -- ^ An imprecise error, when we expect a number of different things,
+                             --   but parse something else.
+  deriving Show
+
+errorPos :: Error -> Pos
+errorPos (Precise p _)   = p
+errorPos (Imprecise p _) = p
+
+-- | Merge two errors. Inner errors (which were thrown at points with more consumed inputs)
+--   are preferred. If errors are thrown at identical input positions, we prefer precise errors
+--   to imprecise ones.
+--
+--   The point of prioritizing inner and precise errors is to suppress the deluge of "expected"
+--   items, and instead try to point to a concrete issue to fix.
+merge :: Error -> Error -> Error
+merge e e' = case (errorPos e, errorPos e') of
+  (p, p') | p < p' -> e'
+  (p, p') | p > p' -> e
+  (p, p')          -> case (e, e') of
+    (Precise{}      , _               ) -> e
+    (_              , Precise{}       ) -> e'
+    (Imprecise _ es , Imprecise _ es' ) -> Imprecise p (es ++ es')
+{-# noinline merge #-} -- merge is "cold" code, so we shouldn't inline it.
+
+-- | Imprecise cut: we slap a list of items on inner errors.
+cut :: Parser a -> [Expected] -> Parser a
+cut p es = do
+  pos <- getPos
+  FP.cutting p (Imprecise pos es) merge
+
+-- | Precise cut: we propagate at most a single error.
+cut' :: Parser a -> Expected -> Parser a
+cut' p e = do
+  pos <- getPos
+  FP.cutting p (Precise pos e) merge
+
+-- | Parse a line comment.
+lineComment :: Parser ()
+lineComment =
+  withOption anyWord8
+    (\case 10 -> ws
+           _  -> lineComment)
+    (pure ())
+
+-- | Parse a potentially nested multiline comment.
+multilineComment :: Parser ()
+multilineComment = go (1 :: Int) where
+  go 0 = ws
+  go n = $(switch [| case _ of
+    "-}" -> go (n - 1)
+    "{-" -> go (n + 1)
+    _    -> branch anyWord8 (go n) (pure ()) |])
+
+-- | Consume whitespace.
+ws :: Parser ()
+ws = $(switch [| case _ of
+  " "  -> ws
+  "\n" -> ws
+  "\t" -> ws
+  "\r" -> ws
+  "--" -> lineComment
+  "{-" -> multilineComment
+  _    -> pure () |])
+
+-- | Consume whitespace after running a parser.
+token :: Parser a -> Parser a
+token p = p <* ws
+{-# inline token #-}
+
+-- | Read a starting character of an identifier.
+identStartChar :: Parser Char
+identStartChar = satisfyAscii isLatinLetter
+{-# inline identStartChar #-}
+
+-- | Read a non-starting character of an identifier.
+identChar :: Parser Char
+identChar = satisfyAscii (\c -> isLatinLetter c || isDigit c)
+{-# inline identChar #-}
+
+-- | Check whether a `Span` contains exactly a keyword. Does not change parsing state.
+isKeyword :: Span -> Parser ()
+isKeyword span = inSpan span do
+  $(FP.switch [| case _ of
+      "lam"   -> pure ()
+      "let"   -> pure ()
+      "in"    -> pure ()
+      "if"    -> pure ()
+      "then"  -> pure ()
+      "else"  -> pure ()
+      "true"  -> pure ()
+      "false" -> pure ()  |])
+  eof
+
+-- | Parse a non-keyword string.
+symbol :: String -> Q Exp
+symbol str = [| token $(FP.string str) |]
+
+-- | Parser a non-keyword string, throw precise error on failure.
+symbol' :: String -> Q Exp
+symbol' str = [| $(symbol str) `cut'` Lit str |]
+
+-- | Parse a keyword string.
+keyword :: String -> Q Exp
+keyword str = [| token ($(FP.string str) `notFollowedBy` identChar) |]
+
+-- | Parse a keyword string, throw precise error on failure.
+keyword' :: String -> Q Exp
+keyword' str = [| $(keyword str) `cut'` Lit str |]
--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,30 @@
 module Main where
 
+import Lexer
+
 import Language.Haskell.HLint
 import GHC.Hs
 import GHC.Types.SrcLoc
-import GHC.Utils.Outputable
+import GHC.Utils.Outputable (defaultSDocContext, ppr, printSDocLn, sdocPprDebug)
 import GHC.Utils.Ppr
+import System.Environment
 import System.IO
+
+-- Flatparse
+import FlatParse.Basic hiding (Parser, runParser, string, char, cut)
+import qualified FlatParse.Basic as FP
+import qualified Data.ByteString as B
+import GHC.Data.FastString
+import qualified GHC.Data.Strict as S
+import GHC.Unit.Module.Name
 
 main :: IO ()
 main = do
-    result <- useHlint "test/files/Parser.hs"
+    args <- getArgs
+    result <- case head args of
+        "h" -> useHlint $ args!!1
+        "f" -> useFlatparse $ args!!1
+        _ -> return Nothing
     case result of
         Just m -> printModule m
         Nothing -> putStrLn "Failed to parse module"
@@ -27,10 +42,79 @@ printModule x = printSDocLn sdocCtx (PageMode True) stdout $ ppr x
 useHlint :: String -> IO (Maybe (Located HsModule))
 useHlint file = do
     (flags, _, _) <- autoSettings
-    x <- parseModuleEx' fileToModule flags file Nothing
+    x <- parseModuleEx flags file Nothing
     case x of
         Right m -> return $ Just $ ghcModule m
         Left _ -> return Nothing
 
 ----------------------------------------
--- 
+-- Flatparse
+
+useFlatparse :: String -> IO (Maybe (Located HsModule))
+useFlatparse file = do
+    str <- B.readFile file
+    case FP.runParser fMod str of
+        Err _  -> do
+            putStrLn "err"
+            return Nothing
+        OK a _ -> return $ Just a
+        Fail   -> do
+            putStrLn "uncaught parse error"
+            return Nothing
+
+fMod :: Parser (Located HsModule)
+fMod = do
+    $(keyword "module")
+    x <- ident'
+    $(keyword "where")
+    d1 <- fDecl
+    return $ mkGeneralLocated "1:1" (hsMod { hsmodName = Just (mkModName x)
+                                           , hsmodDecls = [d1]
+                                           })
+  where
+    hsMod = HsModule
+        { hsmodAnn = EpAnnNotUsed
+        , hsmodName = Nothing
+        , hsmodLayout = VirtualBraces 1
+        , hsmodExports = Nothing
+        , hsmodImports = []
+        , hsmodDecls = []
+        , hsmodDeprecMessage = Nothing
+        , hsmodHaddockModHeader = Nothing
+        }
+    mkModName bs = L
+        (SrcSpanAnn  { ann = EpAnnNotUsed
+                     , locA = RealSrcSpan
+                                (mkRealSrcSpan (mkRealSrcLoc "test/files/Basic.hs" 1 8)
+                                               (mkRealSrcLoc "b" 1 12))
+                                S.Nothing
+        })
+        (mkModuleNameFS (mkFastStringByteList (B.unpack bs)))
+
+fDecl :: Parser (LHsDecl GhcPs)
+fDecl = do
+    return $ mkHsDecl
+  where
+    spn =
+        (SrcSpanAnn  { ann = EpAnnNotUsed
+                     , locA = RealSrcSpan
+                                (mkRealSrcSpan (mkRealSrcLoc "test/files/Basic.hs" 1 8)
+                                               (mkRealSrcLoc "b" 1 12))
+                                S.Nothing
+        })
+    mkHsDecl = L
+        spn
+        (SigD NoExtField (TypeSig EpAnnNotUsed [] (HsWC NoExtField (L spn (HsSig NoExtField
+                                                                        (HsOuterImplicit NoExtField)
+                                                                        (L spn (HsStarTy NoExtField True))
+                                                                        )))))
+
+-- | Parse an identifier. This parser uses `isKeyword` to check that an identifier is not a
+--   keyword.
+ident :: Parser B.ByteString
+ident = token $ byteStringOf $
+  withSpan (identStartChar *> skipMany identChar) (\_ spn -> fails (isKeyword spn))
+
+-- | Parse an identifier, throw a precise error on failure.
+ident' :: Parser B.ByteString
+ident' = ident `cut'` (Msg "identifier")
